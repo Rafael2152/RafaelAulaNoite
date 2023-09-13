586a05a8aa7e5c2d60e306298d583129aba82d91
Rafael2152 <144861634+Rafael2152@users.noreply.github.com>
2023-09-13 19:19:22 -0300

    Add files via upload

---
+++ Condicionais/index.js
@@ -0,0 +1,20 @@
+
+
+const concluiEnsinoMedio = prompt ("voce completou ensino medio")
+const maiorIdade = Number (prompt("Qual sua idade"))
+const cursandoFaculdade = prompt("Voce esta cursando faculdade")
+
+if( concluiEnsinoMedio === "Sim" && maiorIdade >= 18 && cursandoFaculdade === "Sim"){
+
+console.log("voce pode cursar a faculdade")
+    
+}
+else {   console.log("voce nao pode cursar a faculdade")
+}
+
+
+
+function verificaFaculdade(concluiEnsinoMedio,maiorIdade,cursandoFaculdade){
+    console.log(verificaFaculdade)
+}
+
